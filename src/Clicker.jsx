70205c7d532e3ec9ca9283207937e1866ed4605f
Saif Uddin <saifA.uddiN@gmail.com>
2024-08-14 10:31:50 -0700

    awegs

--- src/Clicker.jsx
+++ src/Clicker.jsx
@@ -2,11 +2,15 @@ function handleClick() {
     console.log('clicked you hehe');
 }
 
+function handleHover() {
+    console.log('hovered');
+}
+
 export default function Clicker() {
     return (
         <div>
             <p>Click the button</p>
-            <button onClick={handleClick}>Click</button>
+            <button onMouseOver={handleHover} onClick={handleClick}>Click</button>
         </div>
     )
 }
